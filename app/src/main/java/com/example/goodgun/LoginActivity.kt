a34423a5e17b913377ed5b8fb000428571bed143
JuYongeun <65019047+jye326@users.noreply.github.com>
2023-08-01 17:05:35 +0900

    Add google login button

--- app/src/main/java/com/example/goodgun/LoginActivity.kt
+++ app/src/main/java/com/example/goodgun/LoginActivity.kt
@@ -1,25 +1,55 @@
 package com.example.goodgun
 
+import android.R
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.goodgun.databinding.LoginLayoutBinding
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private var googleSignInClient : GoogleSignInClient ?= null
+    var GOOGLE_LOGIN_CODE = 9001
+
     lateinit var binding:LoginLayoutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = LoginLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
         auth = Firebase.auth
+        var google_sign_in_button = binding.googleLoginBtn
+
+        setGoogleButtonText(google_sign_in_button, "Google 계정으로 로그인")
+
+        google_sign_in_button.setOnClickListener {
+            googleLogin()
+        }
+
+        //구글 로그인을 위해 구성되어야 하는 코드 (Id, Email request)
+        val default_web_client_id = "757404398715-jm0ar4ksakrsdo1jqqe4i7s8f62hoq79.apps.googleusercontent.com"
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(default_web_client_id)
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        //default_web_client_id 이 값은 build 를 해야 value.xml 파일에서 긁어올 수 있어요.
+        //오류 뜨더라도 build 한 번 해주세요
 
 
         //회원가입으로 이동
@@ -29,8 +59,61 @@ class LoginActivity : AppCompatActivity() {
         binding.loginBtn.setOnClickListener {
             signIn(binding.idInput.text.toString(),binding.pwInput.text.toString())
         }
-
     }
+
+    private fun setGoogleButtonText(loginButton: SignInButton, buttonText: String){
+        var i = 0
+        while (i < loginButton.childCount){
+            var v = loginButton.getChildAt(i)
+            if (v is TextView) {
+                var tv = v
+                tv.setText(buttonText)
+                tv.setGravity(Gravity.CENTER)
+                return
+            }
+            i++
+
+        }
+    }
+
+    private fun googleLogin() {
+        var signInIntent = googleSignInClient?.signInIntent
+        if (signInIntent != null) {
+            startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+        }
+    } // googleLogin
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result != null) {
+                if(result.isSuccess) {
+                    var account = result.signInAccount
+                    firebaseAuthWithGoogle(account)
+                }
+            }
+        } //if
+    } // onActivityResult
+
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful) {
+                    // 로그인 성공 시
+                    Toast.makeText(this,  "success", Toast.LENGTH_LONG).show()
+                    startActivity(Intent (this, MainActivity::class.java))
+                    finish()//로그인 엑티비티는 종료
+                } else {
+                    // 로그인 실패 시
+                    Toast.makeText(this,  task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    } //firebaseAuthWithGoogle
+
+
     private fun signIn(ID: String, password: String) {
 
         if (ID.isNotEmpty() && password.isNotEmpty()) {
@@ -55,7 +138,7 @@ class LoginActivity : AppCompatActivity() {
     fun moveMainPage(user: FirebaseUser?){
         if( user!= null){
             startActivity(Intent(this,MainActivity::class.java))
-            finish()
+            finish()    //로그인 페이지는 종료
         }
     }
 }
