f64526b8fa95b3f179e0d18817f1aa495b499bb3
ajroot5685 <anjeong8577@naver.com>
2023-08-02 14:28:13 +0900

    feat: Food 데이터 클래스 정의
    LocalDate 를 사용하려면 sdk 버전 26이상부터 가능함

---
+++ app/src/main/java/com/example/goodgun/Food.kt
@@ -0,0 +1,21 @@
+package com.example.goodgun
+
+import java.time.LocalDate
+
+data class Food(
+    var name: String,
+    var registerDate: LocalDate,
+    var calory: Int,
+    var carbohydrates: Int,
+    var sugar: Int,
+    var protein: Int,
+    var fat: Int,
+    var trans_fat: Int,
+    var saturated_fat: Int,
+    var cholesterol: Int,
+) {
+    // LocalDate 타입은 sdk 버전 26 이상부터 작동하므로 minSdk를 26 이상으로 설정해야함
+    // 기본 생성자
+    // 이름, 등록일자, 열량, 탄수화물, 당류, 단백질, 지방, 트랜스지방, 포화지방, 콜레스테롤
+    constructor() : this("", LocalDate.now(), 0, 0, 0, 0,0,0,0,0)
+}
