0caf6fff253cb38e8d65fcb4366412a8d41dfb7f
ajroot5685 <anjeong8577@naver.com>
2023-08-02 16:39:44 +0900

    feat: 음식 추가 recyclerview, adapter 추가

--- app/src/main/java/com/example/goodgun/Food.kt
+++ app/src/main/java/com/example/goodgun/Food.kt
@@ -4,7 +4,6 @@ import java.time.LocalDate
 
 data class Food(
     var name: String,
-    var registerDate: LocalDate,
     var calory: Int,
     var carbohydrates: Int,
     var sugar: Int,
@@ -13,9 +12,11 @@ data class Food(
     var trans_fat: Int,
     var saturated_fat: Int,
     var cholesterol: Int,
+    var registerDate: LocalDate,
 ) {
     // LocalDate 타입은 sdk 버전 26 이상부터 작동하므로 minSdk를 26 이상으로 설정해야함
     // 기본 생성자
     // 이름, 등록일자, 열량, 탄수화물, 당류, 단백질, 지방, 트랜스지방, 포화지방, 콜레스테롤
-    constructor() : this("", LocalDate.now(), 0, 0, 0, 0,0,0,0,0)
+    constructor() : this("",  0, 0, 0, 0,0,0,0,0,LocalDate.now())
+    constructor(name: String) : this(name,  0, 0, 0, 0,0,0,0,0,LocalDate.now())
 }
--- app/src/main/java/com/example/goodgun/add_food/ScanInfomation.kt
+++ app/src/main/java/com/example/goodgun/add_food/ScanInfomation.kt
@@ -1,12 +1,55 @@
 package com.example.goodgun.add_food
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.goodgun.Food
 import com.example.goodgun.R
+import com.example.goodgun.databinding.ActivityScanInfomationBinding
 
 class ScanInfomation : AppCompatActivity() {
+
+    lateinit var binding: ActivityScanInfomationBinding
+    var tmpdata:ArrayList<Food> = ArrayList()
+    var foodAddData:ArrayList<Food> = ArrayList()
+    lateinit var adapter: FoodAddAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scan_infomation)
+        binding = ActivityScanInfomationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.recyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        tmpdata.apply {
+            add(Food("data1"))
+            add(Food("data2"))
+            add(Food("data3"))
+            add(Food("data4"))
+            add(Food("data5"))
+        }
+
+        adapter = FoodAddAdapter(tmpdata)
+        adapter.itemadd = object :FoodAddAdapter.OnItemClickListener{
+            override fun onItemClick(data: Food, position: Int) {
+                foodAddData.add(data)
+                binding.recyclerView.findViewHolderForAdapterPosition(position)?.itemView?.findViewById<ImageButton>(R.id.food_add)?.visibility = View.GONE
+            }
+        }
+        adapter.itemdelete=object :FoodAddAdapter.OnItemClickListener{
+            // 2번째 onclick 이벤트리스너
+            override fun onItemClick(data: Food, position: Int) {
+                tmpdata.remove(data)
+                adapter.notifyItemRemoved(position)
+            }
+        }
+        binding.recyclerView.adapter = adapter
     }
 }
