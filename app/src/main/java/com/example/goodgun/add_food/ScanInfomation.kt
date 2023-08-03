b8d977e2b5ae97d7242dd643fe0c3a20ba64bdc3
ajroot5685 <anjeong8577@naver.com>
2023-08-03 23:29:30 +0900

    feat: 등록하기 버튼 누를 시 파베DB에 등록

--- app/src/main/java/com/example/goodgun/add_food/ScanInfomation.kt
+++ app/src/main/java/com/example/goodgun/add_food/ScanInfomation.kt
@@ -1,16 +1,23 @@
 package com.example.goodgun.add_food
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.goodgun.MainActivity
 import com.example.goodgun.R
+import com.example.goodgun.User
 import com.example.goodgun.databinding.ActivityScanInfomationBinding
 import com.example.goodgun.roomDB.DatabaseManager
 import com.example.goodgun.roomDB.FoodDatabase
 import com.example.goodgun.roomDB.FoodEntity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -22,13 +29,15 @@ class ScanInfomation : AppCompatActivity() {
     var tmpdata:ArrayList<FoodEntity> = ArrayList()
     lateinit var adapter: FoodAddAdapter
     lateinit var roomdb: FoodDatabase
+    lateinit var database: DatabaseReference
+
+    // 임시 유저 아이디 - 유저 토큰이나 아이디 사용
+    var userid = "2"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityScanInfomationBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // 임시 유저 아이디 - 유저 토큰이나 아이디 사용
-        var userid = "2"
 
         // db 연결
         roomdb = DatabaseManager.getDatabaseInstance(userid, applicationContext)
@@ -37,6 +46,9 @@ class ScanInfomation : AppCompatActivity() {
             roomdb.foodDao().deleteAll()
         }
 
+        database = Firebase.database("https://goodgun-4740f-default-rtdb.firebaseio.com/").reference
+
+        initBtn()
         initRecyclerView()
 
         // 임시 룸DB 확인
@@ -48,6 +60,21 @@ class ScanInfomation : AppCompatActivity() {
                     Toast.makeText(this@ScanInfomation, message, Toast.LENGTH_SHORT).show()
                 }
             }
+        }
+    }
+
+    private fun initBtn() {
+        binding.registerFoods.setOnClickListener {
+            GlobalScope.launch(Dispatchers.IO) {
+                val foods: List<FoodEntity> = roomdb.foodDao().getAll()
+                for (food in foods) {
+                    database.child("user_list").child(userid).child("food")
+                        .setValue(food)
+                }
+                roomdb.foodDao().deleteAll()
+            }
+            startActivity(Intent (this, MainActivity::class.java))
+            finish()
         }
     }
 
