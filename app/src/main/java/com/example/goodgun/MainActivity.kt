93c461ce2dc7b9e5760aa8a19401420fc97624ef
sw19_sungpil <philip000412@gmail.com>
2023-08-05 16:10:44 +0900

    음식 인식

--- app/src/main/java/com/example/goodgun/MainActivity.kt
+++ app/src/main/java/com/example/goodgun/MainActivity.kt
@@ -1,24 +1,17 @@
 package com.example.goodgun
 
 import android.graphics.BitmapFactory
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.JsonReader
 import android.util.Log
 import com.doinglab.foodlens.sdk.FoodLens
 import com.doinglab.foodlens.sdk.LanguageConfig
-import com.doinglab.foodlens.sdk.NutritionRetrieveMode
 import com.doinglab.foodlens.sdk.RecognizeResultHandler
 import com.doinglab.foodlens.sdk.errors.BaseError
 import com.doinglab.foodlens.sdk.network.model.Food
 import com.doinglab.foodlens.sdk.network.model.FoodPosition
 import com.doinglab.foodlens.sdk.network.model.RecognitionResult
-import com.doinglab.foodlens.sdk.ui.util.FoodLensDBManager.init
 import com.example.goodgun.databinding.ActivityMainBinding
-import com.google.gson.GsonBuilder
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -48,9 +41,8 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onError(errorReason: BaseError?) {
-                Log.e("FoodLens", "${errorReason!!.message} dfdalksjdf")
+                Log.e("FoodLens", "${errorReason!!.message}")
             }
-
         })
     }
 }
