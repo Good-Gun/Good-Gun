ca248e9ef5f2453bfe80cdbc3037b2adb22a7e47
Seokjun Lee <seokjun2000@konkuk.ac.kr>
2023-08-09 22:43:30 +0900

    nutrient customization ver2

--- app/src/main/java/com/example/goodgun/login/LoginActivity.kt
+++ app/src/main/java/com/example/goodgun/login/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.view.Gravity
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.goodgun.ApplicationClass
 import com.example.goodgun.MainActivity
 import com.example.goodgun.User
 import com.example.goodgun.databinding.LoginLayoutBinding
